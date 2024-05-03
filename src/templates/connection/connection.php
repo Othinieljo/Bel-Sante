8e475d3fa80abdba7dc2d9c502d8092951544207
Othinieljo <Othinieljo@gnail.com>
2024-05-03 11:39:24 +0000

    Upload some files

--- src/templates/connection/connection.php
+++ src/templates/connection/connection.php
@@ -7,11 +7,16 @@
     <title>Bienvenue sur votre page de connexion | bel'Santé</title>
 </head>
 <body>
+    <div class="loader-wrapper">
+        <div class="back"></div>
+        <div class="heart"></div>
+    </div>
+
     <section class="main">
         <div class="right"></div>
         <div class="left">
-            <form action="index.php?action=login" method="post">
-                <h1 class="gray" style="font-size: 2rem;">Bienvenue</h1>
+            <form action="" method="">
+                <h1 id="texteAnimé" class="gray" style="font-size: 2rem;"></h1>
                 <p>Veuillez vous connecter à votre page d'Administration</p>
                 <hr class="gray">
                 <div>
@@ -22,9 +27,32 @@
                     <label for="password">Mot de passe</label>
                     <input type="password" name="password">
                 </div>
-                <button type="submit" name="login">Se connecter</button>
+                <button type="button">Se connecter</button>
             </form>
         </div>
     </section>
+
+    <script>
+        setTimeout(() => {
+            document.querySelector('.loader-wrapper').style.display = 'none';
+            
+            const texte = "Bienvenue";
+            const delai = 300;
+
+            const afficherTexteLettreParLettre = (texte, delai) => {
+                let index = 0;
+                const afficherLettre = () => {
+                    document.getElementById('texteAnimé').textContent += texte[index];
+                    index++;
+                    if (index < texte.length) {
+                        setTimeout(afficherLettre, delai);
+                    }
+                };
+                afficherLettre();
+            };
+
+            afficherTexteLettreParLettre(texte, delai);
+        }, 3000);
+    </script>
 </body>
 </html>
